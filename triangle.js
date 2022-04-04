242fa152866847eea6a4d3302c2c4f77a9ea1579
Christian Bohollo Sáez <7813599+hondilla@users.noreply.github.com>
2022-04-05 01:09:15 +0200

    refactor

--- triangle.js
+++ triangle.js
@@ -1,7 +1,7 @@
 const calculatePascalTriangle = (n, rows = [[1]]) => {
   if (n < 2 || isNaN(n)) return rows;
   const lastRow = rows[rows.length - 1];
-  const newRow = lastRow.map((item, index) => (index === 0) ? 1 : lastRow[index - 1] + lastRow[index]).concat(1);
+  const newRow = lastRow.map((value, index) => (index === 0) ? 1 : lastRow[index - 1] + value).concat(1);
   return calculatePascalTriangle(n - 1, [...rows, newRow]);
 }
 console.table(calculatePascalTriangle(process.argv.slice(2)));
