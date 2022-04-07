87b4fd3809f7c1cecd6580c43013703689cb2614
Christian Bohollo Sáez <7813599+hondilla@users.noreply.github.com>
2022-04-07 17:40:10 +0200

    refactors

--- README.md
+++ README.md
@@ -1,6 +1,15 @@
 # Pascals Triangle
+## Full Recursive
 
 Run:
 ```
-node triangle.js 9
+node triangle.js 10
+```
+
+## With Tail Cat
+[Wiki](https://en.wikipedia.org/wiki/Tail_call)
+
+Run:
+```
+node triangle-tail-cat.js 10
 ```
--- triangle-tail-cat.js
+++ triangle-tail-cat.js
@@ -1,12 +1,11 @@
-const calculatePascalTriangle = (height) => {
+const calculatePascalTriangle = (height, rows = [[1]]) => {
   if(typeof height !== 'number' || isNaN(height) || height < 1) { 
     return null;
   } 
   if (height == 1) {
-    return [[1]];
-  }  
-  const rows = calculatePascalTriangle(height - 1);
+    return rows;
+  }
   const newRow = rows.at(-1).map((value, index, lastRow) => (index === 0) ? 1 : lastRow[index - 1] + value);
-  return [...rows, [...newRow, 1]];
+  return calculatePascalTriangle(height - 1, [...rows, [...newRow, 1]]);
 }
 console.table(calculatePascalTriangle(parseInt(process.argv.slice(2))));
